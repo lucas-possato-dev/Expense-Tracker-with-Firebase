a10144d2356e4ef4194d2426b671b9b14047f593
lucas-possato-dev <lucaspstinho@gmail.com>
2023-07-06 13:01:38 -0300

    finalizado

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
 "use client";
 import { useState, useEffect } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  querySnapshot,
+  query,
+  onSnapshot,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "./firebase";
 
 export default function Home() {
@@ -12,6 +21,7 @@ export default function Home() {
 
   const [total, setTotal] = useState(0);
   const [newItem, setNewItem] = useState({ name: "", price: "" });
+  const [loading, setLoading] = useState(true);
 
   const addItem = async (e) => {
     e.preventDefault();
@@ -24,9 +34,35 @@ export default function Home() {
     }
   };
 
-  // Read items from database
+  useEffect(() => {
+    const q = query(collection(db, "items"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let itemsArr = [];
+      querySnapshot.forEach((doc) => {
+        itemsArr.push({ ...doc.data(), id: doc.id });
+      });
+      setItems(itemsArr);
+      setLoading(false);
+    });
 
-  // Delete items from database
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      const totalPrice = items.reduce(
+        (sum, item) => sum + parseFloat(item.price),
+        0
+      );
+      setTotal(totalPrice);
+    };
+
+    calculateTotal();
+  }, [items]);
+
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, "items", id));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -37,7 +73,7 @@ export default function Home() {
             <input
               value={newItem.name}
               onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
-              className="col-span-3 p-3 border"
+              className="col-span-3 p-3 border outline-none"
               type="text"
               placeholder="Enter Item"
             />
@@ -46,7 +82,7 @@ export default function Home() {
               onChange={(e) =>
                 setNewItem({ ...newItem, price: e.target.value })
               }
-              className="col-span-2 p-3 border mx-3"
+              className="col-span-2 p-3 border mx-3 outline-none"
               type="number"
               placeholder="Enter $"
             />
@@ -58,28 +94,41 @@ export default function Home() {
               +
             </button>
           </form>
-          <ul>
-            {items.map((item, id) => (
-              <li
-                className="my-4 w-full flex justify-between bg-slate-950"
-                key={id}
-              >
-                <div className="p-4 w-full flex justify-between ">
-                  <span className="capitalize">{item.name}</span>
-                  <span>${item.price}</span>
-                </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">
-                  X
-                </button>
-              </li>
-            ))}
-          </ul>
+          {loading ? (
+            <div className="flex justify-center mt-8">
+              <div className="lds-ripple">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          ) : (
+            <ul>
+              {items.map((item, id) => (
+                <li
+                  className="my-4 w-full flex justify-between bg-slate-950"
+                  key={id}
+                >
+                  <div className="p-4 w-full flex justify-between ">
+                    <span className="capitalize">{item.name}</span>
+                    <span>${item.price}</span>
+                  </div>
+                  <button
+                    onClick={() => deleteItem(item.id)}
+                    className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-red-700 duration-150 w-16"
+                  >
+                    X
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+
           {items.length < 1 ? (
             ""
           ) : (
             <div className="flex justify-between p-3">
               <span>Total</span>
-              <span>${total}</span>
+              <span>${total.toFixed(2)}</span>
             </div>
           )}
         </div>
